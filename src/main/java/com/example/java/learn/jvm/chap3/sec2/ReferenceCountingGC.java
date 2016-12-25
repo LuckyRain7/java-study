2aae9929c5b215e91d1e4c94813d54c264c97557
Wei Zhou <leonzhouwei@gmail.com>
2016-12-25 16:03:30 +0800

    add more

---
+++ src/main/java/com/example/java/learn/jvm/chap3/sec2/ReferenceCountingGC.java
@@ -0,0 +1,31 @@
+package com.example.java.learn.jvm.chap3.sec2;
+
+/**
+ * 
+ * -verbose:gc -Xms20M -Xmx20M -XX:+PrintGCDetails
+ * 
+ * @author zhouwei
+ *
+ */
+public class ReferenceCountingGC {
+	
+	public Object instance = null;
+	private static final int _1MB = 1024 * 1024;
+	
+	private byte[] bigSize = new byte[2 * _1MB];
+	
+	public static void main(String[] args) throws InterruptedException {
+		ReferenceCountingGC objA = new ReferenceCountingGC();
+		ReferenceCountingGC objB = new ReferenceCountingGC();
+		objA.instance = objB;
+		objB.instance = objA;
+		
+		objA = null;
+		objB = null;
+		
+		// 假设在这行发生 GC, objA 和 objB 是否能被回收？
+		System.gc();
+		Thread.sleep(3000);
+	}
+
+}
---
+++ src/main/java/com/example/java/learn/jvm/chap3/sec6/TestAllocation.java
@@ -0,0 +1,26 @@
+package com.example.java.learn.jvm.chap3.sec6;
+
+/**
+ * 
+ * -verbose:gc -Xms20M -Xmx20M -Xmn10M -XX:+PrintGCDetails -XX:SurvivorRatio=8
+ * 
+ * @author zhouwei
+ *
+ */
+public class TestAllocation {
+	
+	private static final int _1MB = 1024 * 1024;
+
+	public static void main(String[] args) {
+		byte[] alloc1;
+		byte[] alloc2;
+		byte[] alloc3;
+		byte[] alloc4;
+		
+		alloc1 = new byte[2 * _1MB];
+		alloc2 = new byte[2 * _1MB];
+		alloc3 = new byte[2 * _1MB];
+		alloc4 = new byte[4 * _1MB]; // 出现一次 Minor GC
+	}
+	
+}
