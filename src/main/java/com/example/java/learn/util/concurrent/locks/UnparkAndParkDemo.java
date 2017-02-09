ba8602e01cfd5f836d6ee91ff66afb67c72b3f74
Wei Zhou <leonzhouwei@gmail.com>
2017-02-09 21:18:04 +0800

    feat: UnparkAndParkDemo.java unpark() first then park()

--- src/main/java/com/example/java/learn/util/concurrent/locks/UnparkAndParkDemo.java
+++ src/main/java/com/example/java/learn/util/concurrent/locks/UnparkAndParkDemo.java
@@ -2,7 +2,7 @@ package com.example.java.learn.util.concurrent.locks;
 
 import java.util.concurrent.locks.LockSupport;
 
-public class ParkAndUnparkDemo {
+public class UnparkAndParkDemo {
 
 	private static class MyThread extends Thread {
 
@@ -15,32 +15,18 @@ public class ParkAndUnparkDemo {
 
 		@Override
 		public void run() {
-			try {
-				Thread.sleep(15000L);
-			} catch (InterruptedException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
 			System.out.println("Blocker Info: " + LockSupport.getBlocker(thread));
 			LockSupport.unpark(thread);
-			try {
-				// we have to sleep here so that
-				// LockSupport.getBlocker(thread)
-				// returns null
-				Thread.sleep(1L);
-			} catch (InterruptedException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
 			System.out.println("Blocker Info: " + LockSupport.getBlocker(thread));
-			System.out.println("after unpark");
+	        System.out.println("after unpark");
 		}
 	}
-
-	public static void main(String[] args) {
+	
+	public static void main(String[] args) throws InterruptedException {
 		MyThread myThread = new MyThread(Thread.currentThread());
 		myThread.start();
-		System.out.println("before park");
+		Thread.sleep(3000L);
+        System.out.println("before park");
 		LockSupport.park("ParkAndUnparkDemo");
 		System.out.println("after park");
 	}
